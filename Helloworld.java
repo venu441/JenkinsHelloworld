ccbed87a9daf9b1d559e59d2af1fdd7dcda723d6
venu441 <sailuuu.ryali@gmail.com>
2022-02-12 01:47:33 +0000

    first commit

--- Helloworld.java
+++ Helloworld.java
@@ -1,5 +1,5 @@
 public class Helloworld{
 public static void main(String[] args){
-System.out.println("......******Helloworld&welcome to jenkins newproject******......");
+System.out.println("......******Helloworld&welcome to jenkins newproject on febuary 12******......");
 }
 }
